f2aa44c52cc547da10bb1f5e5d0a459021e0c509
jounaidr <jounaidruhomaun@googlemail.com>
2020-09-29 16:52:22 +0100

    Spring initializer initial commit

---
+++ src/main/java/com/jounaidr/jrc/node/JrcNodeApplication.java
@@ -0,0 +1,13 @@
+package com.jounaidr.jrc.node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrcNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrcNodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jounaidr/jrc/node/JrcNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jounaidr.jrc.node;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrcNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
