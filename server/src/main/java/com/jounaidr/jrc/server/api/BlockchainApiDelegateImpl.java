1df21b5cb0208f7005cc269ab1ca0c82e4cad566
jounaidr <jounaidruhomaun@googlemail.com>
2020-12-25 18:13:56 +0000

    endpoints added for lastblock and blockchain length
    Signed-off-by: jounaidr <jounaidruhomaun@googlemail.com>

--- server/src/main/java/com/jounaidr/jrc/server/api/BlockchainApiDelegateImpl.java
+++ server/src/main/java/com/jounaidr/jrc/server/api/BlockchainApiDelegateImpl.java
@@ -19,19 +19,39 @@ public class BlockchainApiDelegateImpl implements BlockchainApiDelegate {
         ArrayList<Block> response = new ArrayList<>();
 
         for(int i=0; i < this.blockchain.getChain().size(); i++){
-            Block block = new Block();
-
-            block.setHash(this.blockchain.getChain().get(i).getHash());
-            block.setPreviousHash(this.blockchain.getChain().get(i).getPreviousHash());
-            block.setData(this.blockchain.getChain().get(i).getData());
-            block.setTimeStamp(this.blockchain.getChain().get(i).getTimeStamp());
-            block.setNonce(this.blockchain.getChain().get(i).getNonce());
-            block.setDifficulty(this.blockchain.getChain().get(i).getDifficulty());
-            block.setProofOfWork(this.blockchain.getChain().get(i).getProofOfWork());
-
+            Block block = getBlockResponse(i);
             response.add(block);
         }
 
         return ResponseEntity.ok(response);
+    }
+
+    @Override
+    public ResponseEntity<Block> getLastBlock() {
+        int lastBlockIndex = this.blockchain.getChain().size() - 1;
+        Block response = getBlockResponse(lastBlockIndex);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @Override
+    public ResponseEntity<Integer> getBlockchainLength() {
+        Integer response = this.blockchain.getChain().size();
+
+        return ResponseEntity.ok(response);
+    }
+
+    private Block getBlockResponse(int blockIndex) {
+        Block block = new Block();
+
+        block.setHash(this.blockchain.getChain().get(blockIndex).getHash());
+        block.setPreviousHash(this.blockchain.getChain().get(blockIndex).getPreviousHash());
+        block.setData(this.blockchain.getChain().get(blockIndex).getData());
+        block.setTimeStamp(this.blockchain.getChain().get(blockIndex).getTimeStamp());
+        block.setNonce(this.blockchain.getChain().get(blockIndex).getNonce());
+        block.setDifficulty(this.blockchain.getChain().get(blockIndex).getDifficulty());
+        block.setProofOfWork(this.blockchain.getChain().get(blockIndex).getProofOfWork());
+
+        return block;
     }
 }
